df783359fa8f46abfd935f42fe8551bd415f0000
kaihang-jiang <86507409+kaihang-jiang@users.noreply.github.com>
2025-11-15 23:29:09 +0800

    Update main_IAPRTC.m

--- main_IAPRTC.m
+++ main_IAPRTC.m
@@ -37,16 +37,16 @@ for bi = 1:length(nbits)
                     YTest = T_te;
                     LTest = L_te;
                     
-                    BHSHparam.lambda = lambda(j);
-                    BHSHparam.muta = muta(k);
-                    BHSHparam.theta= theta(h);
-                    BHSHparam.nbits = nbits(bi);
-                    BHSHparam.mbits = ceil(BHSHparam.nbits*0.2);
-                    BHSHparam.beta = beta(r);
-                    BHSHparam.maxItr = maxItr(b);
-                    BHSHparam.func = func;
-                    [B] = CBH(XTrain,YTrain,LTrain',BHSHparam);
-                    eva_info_ = evaluate_CBH(XTrain',YTrain',LTrain,XTest,YTest,LTest,BHSHparam,B);
+                    CBHparam.lambda = lambda(j);
+                    CBHparam.muta = muta(k);
+                    CBHparam.theta= theta(h);
+                    CBHparam.nbits = nbits(bi);
+                    CBHparam.mbits = ceil(CBHparam.nbits*0.2);
+                    CBHparam.beta = beta(r);
+                    CBHparam.maxItr = maxItr(b);
+                    CBHparam.func = func;
+                    [B] = CBH(XTrain,YTrain,LTrain',CBHparam);
+                    eva_info_ = evaluate_CBH(XTrain',YTrain',LTrain,XTest,YTest,LTest,CBHparam,B);
                     eva_info_.Image_VS_Text_MAP;
                     eva_info_.Text_VS_Image_MAP;
                     result.bits = nbits(bi);
@@ -84,4 +84,5 @@ for bi = 1:length(nbits)
 end
 
  end
+
 
